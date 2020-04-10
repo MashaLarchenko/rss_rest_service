e4762f7e7fb5957e6febf775e8b786f1020c57d1
maria larchenko <mashalarchenko16@gmail.com>
2020-04-10 17:12:26 +0300

    feat: add uncaughtException and unhandledRejection

--- src/server.js
+++ src/server.js
@@ -1,5 +1,14 @@
 const { PORT } = require('./common/config');
 const app = require('./app');
+const logger = require('./logger/winstonLogger');
+
+process
+  .on('uncaughtException', err => {
+    logger.error(`Uncaught Exception:${err.message} ${err.stack}`);
+  })
+  .on('unhandledRejection', err => {
+    logger.error(`Unhandled Promise Rejection: ${err.message} ${err.stack}`);
+  });
 
 app.listen(PORT, () =>
   console.log(`App is running on http://localhost:${PORT}`)
