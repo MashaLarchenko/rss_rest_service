849c752427769ffeeab01251a18e3ac30c917f17
maria larchenko <mashalarchenko16@gmail.com>
2020-04-12 16:41:35 +0300

    refactor: change error handling for tasks

--- src/resources/tasks/task.router.js
+++ src/resources/tasks/task.router.js
@@ -1,5 +1,5 @@
 const router = require('express').Router({ mergeParams: true });
-const { OK, NOT_FOUND } = require('http-status-codes');
+const { OK } = require('http-status-codes');
 const Task = require('./task.model');
 const tasksService = require('./task.service');
 const taskSchemas = require('./task.schema');
@@ -18,11 +18,12 @@ router.route('/:id').get(
   catchErrors(async (req, res) => {
     const { boardId, id } = req.params;
     const task = await tasksService.getTaskById(id, boardId);
-    if (task !== undefined) {
-      res.status(OK).json(task);
-    } else {
-      res.status(NOT_FOUND).json(`Task with id ${id} not found`);
-    }
+    // if (task !== undefined) {
+    res.status(OK).json(task);
+    // } else {
+    // throw new ClientErrors('NOT_FOUND');
+    // res.status(NOT_FOUND).json(`Task with id ${id} not found`);
+    // }
   })
 );
 
@@ -38,28 +39,28 @@ router.route('/').post(
 );
 
 router.route('/:id').put(
-  validator.validateSchemaPut(taskSchemas.schemaForPut),
+  catchErrors(validator.validateSchemaPut(taskSchemas.schemaForPut)),
   catchErrors(async (req, res) => {
     const { id, boardId } = req.params;
     const requestData = req.body;
     const task = await tasksService.updateTask(id, boardId, requestData);
-    if (task !== undefined) {
-      res.status(OK).json(task);
-    } else {
-      res.status(NOT_FOUND).json(`Task with id ${id} not found`);
-    }
+    // if (task !== undefined) {
+    res.status(OK).json(task);
+    // } else {
+    // res.status(NOT_FOUND).json(`Task with id ${id} not found`);
+    // }
   })
 );
 
 router.route('/:id').delete(
   catchErrors(async (req, res) => {
     const { id, boardId } = req.params;
-    const task = await tasksService.deleteTask(id, boardId);
-    if (task !== undefined) {
-      res.status(204).json(`Task with id ${id} has been succesfully deleted`);
-    } else {
-      res.status(NOT_FOUND).json(`Task with id ${id} not found`);
-    }
+    await tasksService.deleteTask(id, boardId);
+    // if (task !== undefined) {
+    res.status(204).json(`Task with id ${id} has been succesfully deleted`);
+    // } else {
+    // res.status(NOT_FOUND).json(`Task with id ${id} not found`);
+    // }
   })
 );
 
