16d2bf4d88806d1ab120c19d788baea6d5063fd6
maria larchenko <mashalarchenko16@gmail.com>
2020-04-17 16:33:07 +0300

    refactor: add stacktrace to error logger

--- src/errors/handlers/handleInternalError.js
+++ src/errors/handlers/handleInternalError.js
@@ -2,11 +2,11 @@ const { logger } = require('../../common/logger');
 const { INTERNAL_SERVER_ERROR, getStatusText } = require('http-status-codes');
 
 const handleInternalError = (err, req, res) => {
-  console.log('500');
   logger.error({
     'error code': getStatusText(INTERNAL_SERVER_ERROR),
     url: `Ocured an error in path ${req.originalUrl}`,
-    message: err.message
+    message: err.message,
+    stack: err.stack
   });
   res.status(INTERNAL_SERVER_ERROR).json({
     code: INTERNAL_SERVER_ERROR,
